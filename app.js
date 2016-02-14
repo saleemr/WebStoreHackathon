fe23b56173a4971d85947fa23aec6f93306bb39a
saleemr <sriaz09@gmail.com>
2016-02-14 09:46:50 -0800

    update Index.html and ap.js, an dbook filesd

---
+++ app.js
@@ -0,0 +1,29 @@
+
+var book1 = {
+	name: "How to prepare fo rmarathon?",
+	category: "Sports",
+	price: 14.94,
+	picture: "image_book1",
+	Selling_points: "list"    
+}
+
+
+var book2 = {
+	name: "How to prepare fo rmarathon?",
+	category: "Sports",
+	price: 14.94,
+	picture: "image_book1",
+	Selling_points: "list"    
+}
+
+
+
+
+console.log(book1 + book2)
+
+
+
+
+
+
+
